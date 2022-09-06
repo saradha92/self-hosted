6e92d099116bbfb83986c82a8039df966665cf0b
banuprakash <89842780+epneo-banuprakash@users.noreply.github.com>
2022-09-06 16:19:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-let userGoal = 'Learn Devops-epneo Docker!';
+let userGoal = 'Learn Devops Docker!';
 
 app.use(
   bodyParser.urlencoded({
